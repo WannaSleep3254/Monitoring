641469ac6f3a22fe0f93fe21bdcb9c82df6eb173
JinUk, KIM <31496296+WannaSleep3254@users.noreply.github.com>
2026-05-07 18:08:24 +0900

    1차 수정

--- core/src/FairinoMonitorService.cpp
+++ core/src/FairinoMonitorService.cpp
@@ -81,7 +81,38 @@ struct FairinoMonitorService::Impl
         std::lock_guard<std::mutex> lock(sdk_mutex);
         robot.CloseRPC();
     }
+    // 스냅샷 저장 + 콜백 호출 (공통 로직 분리, pollOnce과 에러 처리에서 모두 사용)
+    void publishSnapshot(RobotSnapshot s)
+    {
+        const auto system_now = std::chrono::system_clock::now();
+        const auto steady_now = std::chrono::steady_clock::now();
 
+        SnapshotCallback temp_cb;
+
+        {
+            std::lock_guard<std::mutex> lock(state_mutex);
+
+            const auto seq = polling_sequence++;
+
+            s.system_timestamp = system_now;
+            s.steady_timestamp = steady_now;
+            s.sequence_number = seq;
+
+            last = s;
+            temp_cb = cb;
+        }
+
+        if (temp_cb) {
+            SnapshotWithMeta meta{
+                s,
+                s.system_timestamp,
+                s.steady_timestamp,
+                s.sequence_number
+            };
+
+            temp_cb(meta);
+        }
+    }
     // fr_test의 while(true) 내용을 1회 실행으로 분리
     void pollOnce()
     {
@@ -91,160 +122,129 @@ struct FairinoMonitorService::Impl
         const auto now_steady = std::chrono::steady_clock::now();
         const auto now_system = std::chrono::system_clock::now();
 
-        int rtn = 0;
+        std::unique_lock<std::mutex> sdk_lock(sdk_mutex, std::try_to_lock);
+        {
+            int rtn = 0;
 
-        // joint pos deg
-        JointPos j_deg{};
-        rtn=robot.GetActualJointPosDegree(opt.robot_id, &j_deg);
-/*
-        if (rtn != 0) {
-            s.connected = false;
-            s.last_error_code = rtn;
-            s.last_error = "GetActualJointPosDegree failed: code="
-                           + std::to_string(rtn);
-            goto error_exit;  // 조기 종료
+            // joint pos deg
+            JointPos j_deg{};
+            rtn=robot.GetActualJointPosDegree(opt.robot_id, &j_deg);
+    /*
+            if (rtn != 0) {
+                s.connected = false;
+                s.last_error_code = rtn;
+                s.last_error = "GetActualJointPosDegree failed: code="
+                               + std::to_string(rtn);
+                goto error_exit;  // 조기 종료
+            }
+    */
+            for (int i=0;i<6;i++) s.joint_pos_deg[i] = j_deg.jPos[i];
+
+            // joint speeds deg
+            float jointSpeed[6] = {0};
+            rtn=robot.GetActualJointSpeedsDegree(opt.robot_id, jointSpeed);
+            for (int i=0;i<6;i++) s.joint_speed_deg[i] = jointSpeed[i];
+
+            // joint acc deg
+            float jointAcc[6] = {0};
+            rtn=robot.GetActualJointAccDegree(opt.robot_id, jointAcc);
+            for (int i=0;i<6;i++) s.joint_acc_deg[i] = jointAcc[i];
+
+            // TCP composite speed (target/actual)
+            float tcp_speed = 0.0f;
+            float ori_speed = 0.0f;
+            rtn=robot.GetTargetTCPCompositeSpeed(opt.robot_id, &tcp_speed, &ori_speed);
+            s.target_tcp_speed_comp = tcp_speed;
+            s.target_ori_speed_comp = ori_speed;
+
+            rtn=robot.GetActualTCPCompositeSpeed(opt.robot_id, &tcp_speed, &ori_speed);
+            s.actual_tcp_speed_comp = tcp_speed;
+            s.actual_ori_speed_comp = ori_speed;
+
+            // TCP speed 6dof (target/actual)
+            float targetSpeed[6] = {0};
+            rtn=robot.GetTargetTCPSpeed(opt.robot_id, targetSpeed);
+            for (int i=0;i<6;i++) s.target_tcp_speed_6[i] = targetSpeed[i];
+
+            float actualSpeed[6] = {0};
+            rtn=robot.GetActualTCPSpeed(opt.robot_id, actualSpeed);
+            for (int i=0;i<6;i++) s.actual_tcp_speed_6[i] = actualSpeed[i];
+
+            // TCP pose
+            DescPose tcp{};
+            rtn=robot.GetActualTCPPose(opt.robot_id, &tcp);
+            s.tcp_pose = { tcp.tran.x, tcp.tran.y, tcp.tran.z, tcp.rpy.rx, tcp.rpy.ry, tcp.rpy.rz };
+
+            // Flange pose
+            DescPose flange{};
+            rtn=robot.GetActualToolFlangePose(opt.robot_id, &flange);
+            s.flange_pose = { flange.tran.x, flange.tran.y, flange.tran.z, flange.rpy.rx, flange.rpy.ry, flange.rpy.rz };
+
+            // tcp/wobj num
+            int id = 0;
+            rtn=robot.GetActualTCPNum(opt.robot_id, &id);
+            s.tcp_num = id;
+
+            rtn=robot.GetActualWObjNum(opt.robot_id, &id);
+            s.wobj_num = id;
+
+            // torques
+            float jtorque[6] = {0};
+            rtn=robot.GetJointTorques(opt.robot_id, jtorque);
+            for (int i=0;i<6;i++) s.joint_torque[i] = jtorque[i];
+
+            // system clock
+            float t_ms = 0.0f;
+            rtn=robot.GetSystemClock(&t_ms);
+            s.system_clock_ms = t_ms;
+
+            // joint config
+            int config = 0;
+            rtn=robot.GetRobotCurJointsConfig(&config);
+            s.joints_config = config;
+
+            // motion done
+            uint8_t motionDone = 0;
+            rtn=robot.GetRobotMotionDone(&motionDone);
+            s.motion_done = motionDone;
+
+            // queue length
+            int len = 0;
+            rtn=robot.GetMotionQueueLength(&len);
+            s.motion_queue_len = len;
+
+            // emergency stop
+            uint8_t emergState = 0;
+            rtn=robot.GetRobotEmergencyStopState(&emergState);
+            s.emergency_stop = emergState;
+
+            // sdk com state
+            int comstate = 0;
+            rtn=robot.GetSDKComState(&comstate);
+            s.sdk_com_state = comstate;
+
+            // safety stop state
+            uint8_t si0 = 0, si1 = 0;
+            rtn=robot.GetSafetyStopState(&si0, &si1);
+            s.safety_si0 = si0;
+            s.safety_si1 = si1;
+
+            // temperatures / driver torque
+            double temp[6] = {0};
+            rtn=robot.GetJointDriverTemperature(temp);
+            for (int i=0;i<6;i++) s.driver_temperature[i] = temp[i];
+
+            double torque[6] = {0};
+            rtn=robot.GetJointDriverTorque(torque);
+            for (int i=0;i<6;i++) s.driver_torque[i] = torque[i];
         }
-*/
-        for (int i=0;i<6;i++) s.joint_pos_deg[i] = j_deg.jPos[i];
-
-        // joint speeds deg
-        float jointSpeed[6] = {0};
-        rtn=robot.GetActualJointSpeedsDegree(opt.robot_id, jointSpeed);
-        for (int i=0;i<6;i++) s.joint_speed_deg[i] = jointSpeed[i];
-
-        // joint acc deg
-        float jointAcc[6] = {0};
-        rtn=robot.GetActualJointAccDegree(opt.robot_id, jointAcc);
-        for (int i=0;i<6;i++) s.joint_acc_deg[i] = jointAcc[i];
-
-        // TCP composite speed (target/actual)
-        float tcp_speed = 0.0f;
-        float ori_speed = 0.0f;
-        rtn=robot.GetTargetTCPCompositeSpeed(opt.robot_id, &tcp_speed, &ori_speed);
-        s.target_tcp_speed_comp = tcp_speed;
-        s.target_ori_speed_comp = ori_speed;
-
-        rtn=robot.GetActualTCPCompositeSpeed(opt.robot_id, &tcp_speed, &ori_speed);
-        s.actual_tcp_speed_comp = tcp_speed;
-        s.actual_ori_speed_comp = ori_speed;
-
-        // TCP speed 6dof (target/actual)
-        float targetSpeed[6] = {0};
-        rtn=robot.GetTargetTCPSpeed(opt.robot_id, targetSpeed);
-        for (int i=0;i<6;i++) s.target_tcp_speed_6[i] = targetSpeed[i];
-
-        float actualSpeed[6] = {0};
-        rtn=robot.GetActualTCPSpeed(opt.robot_id, actualSpeed);
-        for (int i=0;i<6;i++) s.actual_tcp_speed_6[i] = actualSpeed[i];
-
-        // TCP pose
-        DescPose tcp{};
-        rtn=robot.GetActualTCPPose(opt.robot_id, &tcp);
-        s.tcp_pose = { tcp.tran.x, tcp.tran.y, tcp.tran.z, tcp.rpy.rx, tcp.rpy.ry, tcp.rpy.rz };
-
-        // Flange pose
-        DescPose flange{};
-        rtn=robot.GetActualToolFlangePose(opt.robot_id, &flange);
-        s.flange_pose = { flange.tran.x, flange.tran.y, flange.tran.z, flange.rpy.rx, flange.rpy.ry, flange.rpy.rz };
-
-        // tcp/wobj num
-        int id = 0;
-        rtn=robot.GetActualTCPNum(opt.robot_id, &id);
-        s.tcp_num = id;
-
-        rtn=robot.GetActualWObjNum(opt.robot_id, &id);
-        s.wobj_num = id;
-
-        // torques
-        float jtorque[6] = {0};
-        rtn=robot.GetJointTorques(opt.robot_id, jtorque);
-        for (int i=0;i<6;i++) s.joint_torque[i] = jtorque[i];
-
-        // system clock
-        float t_ms = 0.0f;
-        rtn=robot.GetSystemClock(&t_ms);
-        s.system_clock_ms = t_ms;
-
-        // joint config
-        int config = 0;
-        rtn=robot.GetRobotCurJointsConfig(&config);
-        s.joints_config = config;
-
-        // motion done
-        uint8_t motionDone = 0;
-        rtn=robot.GetRobotMotionDone(&motionDone);
-        s.motion_done = motionDone;
-
-        // queue length
-        int len = 0;
-        rtn=robot.GetMotionQueueLength(&len);
-        s.motion_queue_len = len;
-
-        // emergency stop
-        uint8_t emergState = 0;
-        rtn=robot.GetRobotEmergencyStopState(&emergState);
-        s.emergency_stop = emergState;
-
-        // sdk com state
-        int comstate = 0;
-        rtn=robot.GetSDKComState(&comstate);
-        s.sdk_com_state = comstate;
-
-        // safety stop state
-        uint8_t si0 = 0, si1 = 0;
-        rtn=robot.GetSafetyStopState(&si0, &si1);
-        s.safety_si0 = si0;
-        s.safety_si1 = si1;
-
-        // temperatures / driver torque
-        double temp[6] = {0};
-        rtn=robot.GetJointDriverTemperature(temp);
-        for (int i=0;i<6;i++) s.driver_temperature[i] = temp[i];
-
-        double torque[6] = {0};
-        rtn=robot.GetJointDriverTorque(torque);
-        for (int i=0;i<6;i++) s.driver_torque[i] = torque[i];
-
         // (원본 fr_test 마지막) realtime pkg
         // ROBOT_STATE_PKG pkg{};
         // robot.GetRobotRealTimeState(&pkg);
         // -> 필요하면 RobotSnapshot에 pkg에서 뽑아낼 항목을 추가해 확장
         // 콜백
 //error_exit: // 공통 저장 로직 (정상/에러 모두)
-#if false
-        // 최신값 저장
-        {
-            std::lock_guard<std::mutex> lk(mtx);
-            last = s;
-        }
-        if (cb) cb(s);
-#else
-        SnapshotCallback temp_cb;
-        {
-            //std::lock_guard<std::mutex> lk(mtx);
-            std::lock_guard<std::mutex> lock(sdk_mutex);
-            last = s;
-            temp_cb = cb;
-        }
-        if (temp_cb) {
-            // 콜백이 오래 걸릴 수 있으므로 락 밖에서 호출
-            const auto system_now = std::chrono::system_clock::now();
-            const auto steady_now = std::chrono::steady_clock::now();
-            const auto seq = polling_sequence++;
-
-            s.system_timestamp = system_now;
-            s.steady_timestamp = steady_now;
-            s.sequence_number = seq;
-
-            SnapshotWithMeta meta{
-                s,                                      // ✅ RobotSnapshot
-                system_now,
-                steady_now,
-                seq
-            };
-            temp_cb(meta);  // ✅ SnapshotWithMeta 전달
-        }
-#endif
+        publishSnapshot(s);
     }
 
     void loop()
@@ -258,35 +258,8 @@ struct FairinoMonitorService::Impl
                 RobotSnapshot s{};
                 s.connected = false;
                 s.last_error = "Exception in pollOnce()";
-#if false
-                if (cb) cb(s);
-#else
-                SnapshotCallback temp_cb;
-                {
-                    //std::lock_guard<std::mutex> lk(mtx);
-                    std::lock_guard<std::mutex> lock(sdk_mutex);
-                    last = s;
-                    temp_cb = cb;
-                }
 
-                if (temp_cb) {
-                    const auto system_now = std::chrono::system_clock::now();
-                    const auto steady_now = std::chrono::steady_clock::now();
-                    const auto seq = polling_sequence++;
-
-                    s.system_timestamp = system_now;
-                    s.steady_timestamp = steady_now;
-                    s.sequence_number = seq;
-
-                    SnapshotWithMeta meta{
-                        s,                                      // ✅ RobotSnapshot
-                        system_now,
-                        steady_now,
-                        seq
-                    };
-                    temp_cb(meta);
-                }
-#endif
+                publishSnapshot(s);
             }
 
             std::this_thread::sleep_for(std::chrono::milliseconds(opt.poll_period_ms));
@@ -295,6 +268,7 @@ struct FairinoMonitorService::Impl
 
     bool queryStaticInfo()
     {
+        std::lock_guard<std::mutex> lock(sdk_mutex);
         // fr_test의 버전/하드웨어/펌웨어/설치각도 출력 부분
         char robotModel[64] = {0};
         char webversion[64] = {0};
