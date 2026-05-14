2ed1ddbf193f2d946bada7db6640a893e53140b1
JinUk, KIM <31496296+WannaSleep3254@users.noreply.github.com>
2026-05-14 19:24:55 +0900

    Add IoT SQLite database and schema

--- app/src/iot/storage/IotSchema.h
+++ app/src/iot/storage/IotSchema.h
@@ -1,28 +1,2 @@
 #pragma once
 
-#include <QSqlDatabase>
-#include <QString>
-
-class IotDatabase
-{
-public:
-    explicit IotDatabase(const QString& connectionName = "iot_connection");
-    ~IotDatabase();
-
-    bool open(const QString& databasePath = QString());
-    void close();
-
-    bool isOpen() const;
-    QSqlDatabase database() const;
-
-    QString databasePath() const;
-    QString lastError() const;
-
-private:
-    QString defaultDatabasePath() const;
-
-private:
-    QString m_connectionName;
-    QString m_databasePath;
-    QString m_lastError;
-};
